165c718045b07fbda93113330520f3a5d6a0884c
Mirabbos995 <jahonaliyevmirabbos621@gmail.com>
2024-02-16 20:12:44 +0300

    p

---
+++ main.py
@@ -0,0 +1,152 @@
+
+# filter_list = [1, 2, 3, "a", "b", 4]        # 1 - misol
+# flt = []
+# for a in filter_list:
+#     if type(a) == int:
+#         flt.append(a)
+# print(flt)
+
+
+
+# Add the Index   2 - misol
+
+
+
+
+
+
+
+
+
+# Probabilities (Part 1)   3 - misol
+# a = [3,4,5,7,8,17,9,2]
+# b = []
+# for x in a:
+#     if 6 < x:
+#         b.append(x)
+# print(b)
+
+
+                                # 4 - misol
+# def stand():
+#     line = [5, 6, 7, 8, 9]
+#     line.remove(5)
+#     line.append(a)
+#     print(line)
+#     return
+#
+# a = int(input("Enter: "))
+# b = stand()
+
+# # Get Sum of People's Budget   # 5 - misol
+# names = ["Adam", "Sarah", "Malcolm"]
+# table = []
+# for name in names:
+#     table.append(name[0])
+#     table.sort()
+# j = "".join(table)
+# print(j)
+
+
+
+
+
+
+# list = [1,2]       #6 - misol
+# a = []
+# for x in list:
+#     list.copy()
+#     a.append(list)
+# print(a)
+
+
+
+# integer = [9, 2, "space", "car", "lion", 16]              # 7 - misol
+# back = []
+#
+# def ret_int():
+#     for x in integer:
+#         if type(x) == int:
+#             back.append(x)
+#     print(back)
+# ret_int()
+
+
+
+# Chat Room Status          # 8 - misol
+# not done
+
+
+
+# List Operation             # 9 - misol
+# not done
+
+
+
+# Simon Says          # 10 - misol
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = [1,2,3,4,5,6,7,8,9,10]
+# #print(list(enumerate(a)))
+# x = a[1:10:3]
+# print(x)
+
+
+
+# a = [1,2,3,4,5,6,7,8,9,10]
+# [print(x)for x in a if x % 2 == 0]
+
+
+# a = [1,2,2,3,3,4,5,5,6,7,8,9,9]
+# c = []
+# # [c.append(x) for x in a if x not in c]
+# # print(c)
+#
+# for x in a:
+#     if x not in c:
+#         c.append(x)
+# print(c)
+
+
+
+
+
+# lst = [1,3,6,3,9,0,2]
+# lst.sort(reverse=True)
+# print(lst)
+
+
+
+# lst = [1,3,6,3,9,0,2]
+# [print(x * 3) for x in lst]
